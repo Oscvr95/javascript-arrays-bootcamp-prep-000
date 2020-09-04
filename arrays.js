419e5c8268f44485ed17e0f2f49a49891ab6b75e
Oscar Lopez <oscarfrom95@hmail.com>
2020-09-04 15:03:03 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -43,5 +43,5 @@ function destructivelyRemoveElementFromEndOfArray(array1) {
 }
 
 function removeElementFromEndOfArray(array2) {
-  return array2 = array2.slice(0, 3)
+  return array2 = array2.slice(0, array2.length - 1)
 }
